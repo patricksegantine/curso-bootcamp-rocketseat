2a41ca8c2de3e2c7651782e7dd8a6055f76b5da1
Patrick Segantine <patricksegantine@gmail.com>
2020-03-01 23:00:37 -0300

    criando o loader de Models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Patrick' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Patrick Segantine',
+    email: 'patricksegantine@gmail.com',
+    password_hash: '12345678'
+  });
+
+  // return res.json({ message: 'Hello Patrick' });
+  return res.json(user);
 });
 
 export default routes;
