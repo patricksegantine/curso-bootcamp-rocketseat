67e5d0737807db99bb73626b1e083101484c58e1
Patrick Segantine <patricksegantine@gmail.com>
2020-03-01 23:22:43 -0300

    incluindo o controller UserController

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Patrick Segantine',
-    email: 'patricksegantine@gmail.com',
-    password_hash: '12345678'
-  });
-
-  // return res.json({ message: 'Hello Patrick' });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
