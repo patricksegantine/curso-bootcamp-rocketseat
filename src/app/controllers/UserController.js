b359f6cb7b59775f65a36e9b102998ccfc688fe4
Patrick Segantine <patricksegantine@gmail.com>
2020-03-05 22:24:09 -0300

    Revert "bugfix propriedade 'userId' apos autenticacao'"
    This reverts commit 0d0ad9a302205841de1d52500d48093135812fda.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,22 +1,7 @@
-import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const schema = Yup.object().shape({
-      name: Yup.string().required(),
-      email: Yup.string()
-        .email()
-        .required(),
-      password: Yup.string()
-        .required()
-        .min(6)
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      res.status(400).json({ error: 'Validations fail' });
-    }
-
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -37,54 +22,8 @@ class UserController {
     });
   }
 
-  /**
-   * Atualiza os dados do usuário
-   */
   async update(req, res) {
-    const schema = Yup.object().shape({
-      name: Yup.string(),
-      email: Yup.string().email(),
-      oldPassword: Yup.string()
-        .required()
-        .min(6),
-      password: Yup.string()
-        .min(6)
-        .when('oldPassword', (oldPassword, field) =>
-          oldPassword ? field.required() : field
-        ),
-      confirmPassword: Yup.string().when('password', (password, field) =>
-        password ? field.required().oneOf([Yup.ref('password')]) : field
-      )
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      res.status(400).json({ error: 'Validations fail' });
-    }
-
-    const { email, oldPassword } = req.body;
-
-    const user = await User.findByPk(req.userId);
-
-    if (email && email !== user.email) {
-      const userExists = await User.findOne({ where: { email } });
-
-      if (userExists) {
-        return res.status(400).json({ error: 'User already exists' });
-      }
-    }
-
-    if (oldPassword && !(await user.checkPassword(oldPassword))) {
-      return res.status(401).json({ error: 'Password does not match' });
-    }
-
-    const { id, name, provider } = await user.update(req.body);
-
-    return res.json({
-      id,
-      name,
-      email,
-      provider
-    });
+    res.json({ ok: true });
   }
 }
 
