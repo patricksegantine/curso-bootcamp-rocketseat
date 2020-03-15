db3118ea8a686dd1a46081b449a4fffa34ff23d4
Patrick Segantine <patricksegantine@gmail.com>
2020-03-14 22:09:48 -0300

    criando agendamento (appointments)

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleare from './app/middlewares/auth';
@@ -12,6 +13,7 @@ import authMiddleare from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+// ROTAS
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
@@ -21,6 +23,8 @@ routes.use(authMiddleare);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
