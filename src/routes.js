19ca2574c40682f0eb28e8077aef0838b716996f
Patrick Segantine <patricksegantine@gmail.com>
2020-03-22 19:08:26 -0300

    listando horarios do prestador de servico disponiveis

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import AvailableController from './app/controllers/AvailableController';
 import NotificationController from './app/controllers/NotificationController';
 import FileController from './app/controllers/FileController';
 
@@ -25,6 +26,7 @@ routes.use(authMiddleare);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
