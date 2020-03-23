b3e211fca47e1fb54beb9f4a83c99ac23efa571c
Patrick Segantine <patricksegantine@gmail.com>
2020-03-23 00:31:02 -0300

    tratamento de excecoes com youch, sentry

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
-import routes from './routes';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
 
+import sentryConfig from './config/sentry';
+import routes from './routes';
 import './database';
 
 class App {
@@ -11,11 +14,18 @@ class App {
 
     this.server = express();
 
+    /**
+     * application monitoring
+     */
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -25,6 +35,19 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const erros = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(erros);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
